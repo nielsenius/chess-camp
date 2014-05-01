e2b6721e08425c4abdbb57e1dbd695f0ca5f7e33
Matthew Nielsen <mqn@andrew.cmu.edu>
2014-05-01 17:47:50 -0400

    Got authentication working.

--- config/routes.rb
+++ config/routes.rb
@@ -6,12 +6,18 @@ ChessCamp::Application.routes.draw do
   resources :families
   resources :locations
   resources :students
+  resources :sessions
 
   # semi-static routes
   get 'home', to: 'home#index', as: :home
   get 'home/about', to: 'home#about', as: :about
   get 'home/contact', to: 'home#contact', as: :contact
   get 'home/privacy', to: 'home#privacy', as: :privacy
+  
+  # get 'user/edit' => 'users#edit', :as => :edit_current_user
+  # get 'signup', to: 'users#new', as: :signup
+  get 'login', to: 'sessions#new', as: :login
+  get 'logout', to: 'sessions#destroy', as: :logout
 
   # set the root url
   root to: 'home#index'
