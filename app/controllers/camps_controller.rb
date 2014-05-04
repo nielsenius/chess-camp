f48339976e761f353ba5adbbf70d99a96dcdacfd
Matthew Nielsen <mqn@andrew.cmu.edu>
2014-05-04 12:16:51 -0400

    More authorization in controllers.

--- app/controllers/camps_controller.rb
+++ app/controllers/camps_controller.rb
@@ -1,5 +1,8 @@
 class CampsController < ApplicationController
+  
   before_action :set_camp, only: [:show, :edit, :update, :destroy]
+  
+  authorize_resource
 
   def index
     @upcoming_camps = Camp.upcoming.active.chronological.paginate(:page => params[:page]).per_page(10)
