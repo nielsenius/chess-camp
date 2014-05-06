2a0136d165539497e9780bfa57f0919306e4fa24
Matthew Nielsen <mqn@andrew.cmu.edu>
2014-05-06 16:14:29 -0400

    Attempting to debug location logout issue.

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -2,7 +2,7 @@ class LocationsController < ApplicationController
   
   before_action :set_location, only: [:show, :edit, :update, :destroy]
   
-  authorize_resource
+  # authorize_resource
 
   def index
     @active_locations = Location.active.alphabetical.paginate(page: params[:page]).per_page(10)
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,23 +7,15 @@ class Ability
     
     # set authorizations for different user roles
     if user.role? :admin
+      
       # they get to do it all
       can :manage, :all
       
     elsif user.role? :instructor
+      
       # can view their own details
       can :show, Instructor do |i|
         i.id == user.instructor.id
-      end
-      
-      # can see lists of students in their camps
-      can :read, Student do |s|
-        camps = user.instructor.camps
-        camp_students = []
-        camps.each do |camp|
-          camp_students << camp.students.map(&:id)
-        end
-        camp_students.include? s.id
       end
       
       # can their edit info, except role
@@ -32,24 +24,36 @@ class Ability
       end
       cannot :update, User, [:role]
       
+      # can see camps they are assigned to
+      can :read, Camp do |c|
+        instructor_camps = user.instructor.camps.map(&:id)
+        instructor_camps.include? c.id
+      end
+      
+      # can see lists of students in their camps
+      can :read, Student do |s|
+        camp_students = user.instructor.camps.map { |c| c.students.map(&:id) }.flatten
+        camp_students.include? s.id
+      end
+      
     else
       # can read home pages
       
-      # can read active, upcoming camps
-      can :show, Camp, Camp.upcoming do |c|
-        c.active
-      end
       
-      # can read address and map of camps
-      # can :show, Location, 
-      # cannot :read, Location, [:max_capacity]
-      
-      # can read camp instructors' bios and photos
-      can :read, Instructor, [:first_name, :last_name, :bio, :picture]
-      can :read, Instructor do |i|
-        Camp.upcoming.instructors.map(&:id).include? i.id
-      end
-      
+      # # can read active, upcoming camps
+      # can :show, Camp, Camp.upcoming do |c|
+      #   c.active
+      # end
+      # 
+      # # can read address and map of camps
+      # # can :show, Location, 
+      # # cannot :read, Location, [:max_capacity]
+      # 
+      # # can read camp instructors' bios and photos
+      # can :read, Instructor, [:first_name, :last_name, :bio, :picture]
+      # can :read, Instructor do |i|
+      #   Camp.upcoming.instructors.map(&:id).include? i.id
+      # end
     end
   end
 end
