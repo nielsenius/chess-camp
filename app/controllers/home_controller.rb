6344fbdfb290983ac5178d5c4fd933f5f0db5de4
Matthew Nielsen <mqn@andrew.cmu.edu>
2014-05-07 18:09:18 -0400

    Dashboard working.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,14 +3,6 @@ class HomeController < ApplicationController
     @upcoming_camps = Camp.upcoming.active.chronological.paginate(:page => params[:page]).per_page(10)
   end
   
-  def camp_summary
-    @camp = Camp.find(params[:id])
-    @description = @camp.curriculum.description
-    @instructors = @camp.instructors.alphabetical.to_a
-    @registrations = @camp.students.alphabetical.to_a
-    @location = @camp.location
-  end
-  
   def about
   end
 
@@ -18,6 +10,23 @@ class HomeController < ApplicationController
   end
 
   def privacy
+  end
+  
+  def camp_summary
+    @camp = Camp.find(params[:id])
+    @description = @camp.curriculum.description
+    @instructors = @camp.instructors.active.alphabetical.to_a
+    @registrations = @camp.students.alphabetical.to_a
+    @location = @camp.location
+  end
+  
+  def dashboard
+    @instructor = current_user.instructor
+    @my_upcoming_camps = @instructor.camps.active.upcoming.to_a
+    
+    if current_user.role == 'admin'
+      @all_upcoming_camps = Camp.upcoming.active.chronological.paginate(:page => params[:page]).per_page(6)
+    end
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ ChessCamp::Application.routes.draw do
   get 'home/contact', to: 'home#contact', as: :contact
   get 'home/privacy', to: 'home#privacy', as: :privacy
   get 'home/camp_summary/:id', to: 'home#camp_summary', as: :camp_summary
+  get 'home/dashboard', to: 'home#dashboard', as: :dashboard
   
   get 'login', to: 'sessions#new', as: :login
   get 'logout', to: 'sessions#destroy', as: :logout
